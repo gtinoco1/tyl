429fa487c36c9fdaa774532eb5944382bdbf9ce3
Guillermo Tinoco <gtinoco@chicagobooth.edu>
2019-01-07 22:35:36 +0000

    fixed activity_type create

--- app/controllers/activity_types_controller.rb
+++ app/controllers/activity_types_controller.rb
@@ -32,7 +32,7 @@ class ActivityTypesController < ApplicationController
     if @activity_type.valid?
       @activity_type.save
 
-      redirect_back(:fallback_location => "/activity_types", :notice => "Activity type created successfully.")
+      redirect_to("/properties", :notice => "Activity type created successfully.")
     else
       render("activity_type_templates/new_form_with_errors.html.erb")
     end
