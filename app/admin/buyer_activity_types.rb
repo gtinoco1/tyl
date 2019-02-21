8dc09e409d54bd2781580d2e1f309c02929ccf21
Guillermo Tinoco <gtinoco@chicagobooth.edu>
2019-02-21 19:34:05 +0000

    allow params admin

--- app/admin/buyer_activity_types.rb
+++ app/admin/buyer_activity_types.rb
@@ -1,10 +1,10 @@
 ActiveAdmin.register BuyerActivityType do
-  permit_params :user_id, :duration_toggle, :property_address_toggle, :zipcode_toggle, :detail_toggle, :comment_toggle, :image_toggle, :subject_toggle, :agent_toggle
+  permit_params :user_id, :duration_toggle, :property_address_toggle, :zipcode_toggle, :detail_toggle, :comment_toggle, :image_toggle, :subject_toggle, :agent_toggle, :title
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+# permit_params :user_id, :duration_toggle, :property_address_toggle, :zipcode_toggle, :detail_toggle, :comment_toggle, :image_toggle, 
 #
 # or
 #
