05d7d59dc9a170ebf2c6f845bc427f7b7545179e
Guillermo Tinoco <gtinoco@chicagobooth.edu>
2019-02-28 19:20:05 +0000

    # symbol quick fix img uploads

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,13 +3,6 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
-
     # Here is an example of a simple dashboard with columns and panels.
     #
     columns do
@@ -17,8 +10,9 @@ ActiveAdmin.register_page "Dashboard" do
         panel "Users" do
           ul do
             User.all.map do |user|
-              li link_to(user.email, admin_user_path(user))
+              li link_to(user.first_name, admin_user_path(user))
             end
+           para "Total Users: #{User.all.count}"
           end
         end
       end
