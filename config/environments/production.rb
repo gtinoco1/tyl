65f64c54dbd2247cd65ec9c01af87d8a3c8ccb43
Guillermo Tinoco <gtinoco@chicagobooth.edu>
2019-03-01 07:43:05 +0000

    secure protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'www.trackyourlisting.com' }
+  config.action_mailer.default_url_options = { :host => 'www.trackyourlisting.com',
+  protocol: 'https'}
   
   Rails.application.routes.default_url_options[:host] = 'track-your-listing.herokuapp.com'
 
