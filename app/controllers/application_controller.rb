7deffc1de808ce2a819c9da824f9650a9cd1e7ef
Guillermo Tinoco <gtinoco@chicagobooth.edu>
2019-03-01 02:47:26 +0000

    messing with email for password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,5 @@ class ApplicationController < ActionController::Base
   end
   skip_before_action :verify_authenticity_token, raise: false
   
-      before_action :authenticate_user!
+  before_action :authenticate_user!
 end
