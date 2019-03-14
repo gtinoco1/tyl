fd30013d4f7d0c062d228a36c53dbec5fca0a3e6
Guillermo Tinoco <gtinoco@chicagobooth.edu>
2019-03-14 09:02:59 +0000

    added type.id in report_two

--- app/pdfs/report_two_pdf.rb
+++ app/pdfs/report_two_pdf.rb
@@ -30,7 +30,7 @@ class ReportTwoPdf < Prawn::Document
 
     current_user.activity_types.each do |type|
       
-      if @property.activities.where(activity_type_id: type).where(:date => (@start_date..@end_date)).count > 0
+      if @property.activities.where(activity_type_id: type.id).where(:date => (@start_date..@end_date)).count > 0
         eval("method_#{type.id}")
       else
       end
