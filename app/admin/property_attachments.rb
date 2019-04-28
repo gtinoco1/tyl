ede598d0cb9c0f0dc242783570a496a0725b0a6a
Guillermo Tinoco <gtinoco@chicagobooth.edu>
2019-04-28 02:22:17 +0000

    permit params attachment

--- app/admin/property_attachments.rb
+++ app/admin/property_attachments.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register PropertyAttachment do
-  permit_params :property_id, :user_id, :activity_id, :image, :title, :pages
+  permit_params :property_id, :user_id, :activity_id, :image, :title, :pages, :attachment
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
