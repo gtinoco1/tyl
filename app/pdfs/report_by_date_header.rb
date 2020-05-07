e74db99088bc4e34311b54197e2c0f84a0399e85
jigarkakadiya <jka@narola.email>
2020-05-07 18:46:34 +0530

    Change report header logo and secure site for profile page

--- app/pdfs/report_by_date_header.rb
+++ app/pdfs/report_by_date_header.rb
@@ -106,7 +106,7 @@ class ReportByDateHeader < Prawn::Document
     # stroke do
     #   horizontal_rule
     # end
-    image open("https://res.cloudinary.com/hnx8y80mv/image/upload/v1562762929/main/Coldwell_Banker_Logo1.png"), :height => 60, :at => [0, 720]
+    image open("https://res.cloudinary.com/hnx8y80mv/image/upload/v1588853250/main/picturemessage_fnlzoyti.gxy.png"), :height => 40, :width => 150, :at => [0, 720]
 
     if @current_user.headshot.blank?
     else
