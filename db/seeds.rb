934fc3cd194c9ca94f00e282a609dc3d54efb4be
jigarkakadiya <jka@narola.email>
2020-05-16 13:03:52 +0530

    Recaptcha site_key validation to run project in Gitpod

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-AdminUser.create!(email: 'admin123@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+User.create(email: 'user@example.com', password: 'password', password_confirmation: 'password', first_name: 'test', last_name: 'test', confirmed_at: DateTime.now) if Rails.env.development?
