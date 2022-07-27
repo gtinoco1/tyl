cf69d4edb66e110b962a6e7ce29ecabe7cb2db3d
gtinoco1 <Guille.tinoco@gmail.com>
2022-07-27 17:29:52 +0000

    update to admin

--- app/admin/properties.rb
+++ app/admin/properties.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Property do
   actions :all, :except => [:destroy]
   index :download_links => [:csv, :xml, :json]
-  permit_params :address, :realtor_id, :city, :state, :zipcode, :status
+  permit_params :address, :realtor_id, :city, :state, :zipcode, :status, :listing_type
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -15,5 +15,18 @@ ActiveAdmin.register Property do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+csv do
+  column :id
+  column(:user_id) { |property| property.user.email }
+  column(:property_address) { |property| property.address }
+  column(:city) { |property| property.city }
+  column(:state) { |property| property.state }
+  column(:zipcode) { |property| property.zipcode }
+  column(:status) { |property| property.status }
+  column(:created_at) { |property| property.created_at }
+  column(:updated at) { |property| property.updated_at }
+  column(:listing_type) { |property| property.listing_type }
+end
 
 end
